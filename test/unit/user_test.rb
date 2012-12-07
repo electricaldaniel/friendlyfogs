ba1a8872787f482d952bc951541683a0b521bd70
Daniel Svabo <danielsvabo@yahoo.com>
2012-12-07 22:02:00 +0100

    new updates
    new updates

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
+
    test "a user should enter a first name" do
      user = User.new
      assert !user.save
@@ -28,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-    user = User.new(first_name: 'Daniel', last_name: 'Svabo', email: 'dtsvabo@gmail.com')
+    user = User.new(first_name: 'Tobias', last_name: 'Svabo', email: 'chocolate@hotmail.com')
     user.password = user.password_confirmation = 'asdfasdf'
 
     user.profile_name = "My Profile With Spaces"
@@ -39,11 +43,27 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user can have a correctly formatted profile name" do
-    user = User.new(first_name: 'Daniel', last_name: 'Svabo', email: 'dtsvabo@gmail.com')
+    user = User.new(first_name: 'Tobias', last_name: 'Svabo', email: 'chocolate@hotmail.com')
     user.password = user.password_confirmation = 'asdfasdf'
 
     user.profile_name = 'dansva_1'
     assert user.valid?
 
   end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:daniel).friends
+     end
   end
+
+  test "that creating friendships on a user works" do
+    users(:daniel).friends << users(:dirty)
+    users(:daniel).friends.reload
+    assert users(:daniel).friends.include?(users(:dirty))
+  end
+
+  test "that calling to_param on a user returns the profile_name" do
+    assert_equal "dirtyharry", users(:dirty).to_param
+  end
+end
